a09ccb64a6d680c6536278a695be060ecf09103a
ruiha-harawira <ruiha.harawira@hotmail.com>
2022-06-15 11:28:46 +1200

    adding nav

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-
+import continents from '../../data/continents'
+  
 function Nav() {
+
+let continentsArr = Object.keys(continents)
+
   return (
     <div>
       <h2>Nav</h2>
       <ul>
-        <li>Build your continent list here</li>
+    {continentsArr.map((continent, index) => {
+      return <li key = {index}> {continent}</li>
+    })}
       </ul>
     </div>
   )
 }
+
 
 export default Nav
